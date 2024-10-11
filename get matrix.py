ea282b8c95ec9c8378ce7d3f9cefb969f3e71867
Ринат <r3natych@gmail.com>
2024-10-11 15:11:39 +0300

    Update get matrix.py

--- get matrix.py
+++ get matrix.py
@@ -9,7 +9,7 @@ def get_matrix(n,m, value):
     return matrix
 
 result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 0, 42)
+result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 
 print(result1)
